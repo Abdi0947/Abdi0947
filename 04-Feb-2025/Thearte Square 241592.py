80b085617a06d21617ea3c368ce5b98284ef8124
Abdi Debela <122887024+Abdi0947@users.noreply.github.com>
2025-02-04 23:21:26 +0300

    Created 'Thearte Square' by A2SV Hub

---
+++ 04-Feb-2025/Thearte Square 241592.py
@@ -0,0 +1,15 @@
+# Problem: Thearte Square - https://codeforces.com/problemset/problem/1/A
+
+import math
+
+# Input the dimensions of the rectangle and the flagstone size
+n, m, a = map(int, input().split())
+
+# Calculate the number of flagstones needed
+flagstones_x = math.ceil(n / a)
+flagstones_y = math.ceil(m / a)
+
+# Total flagstones
+total_flagstones = flagstones_x * flagstones_y
+
+print(total_flagstones)
