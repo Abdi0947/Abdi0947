c0e89b000bac423941d76e6c0aa33be83b4a3b91
Abdi Debela <122887024+Abdi0947@users.noreply.github.com>
2025-02-04 23:21:06 +0300

    Created 'Way Too Long Words' by A2SV Hub

---
+++ 04-Feb-2025/Way Too Long Words 241589.py
@@ -0,0 +1,16 @@
+# Problem: Way Too Long Words - https://codeforces.com/problemset/problem/71/A
+
+n=int(input())
+for i in range(n):
+    word=input()
+    word=list(word)
+    long=[]
+    k=len(word)
+    if k>10:
+        for i in range(1,len(word)):
+            long.append(word[i])
+        long_words=word[0]+str(len(long)-1)+word[len(word)-1]
+        print(long_words)
+    else:
+        word=''.join(word)
+        print(word)
