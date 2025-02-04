df680a87f6db88da72bb0ae362a31d0cb150a2af
Abdi Debela <122887024+Abdi0947@users.noreply.github.com>
2025-02-04 18:09:41 +0300

    Created 'Grading Students' by A2SV Hub

---
+++ 04-Feb-2025/Grading Students 240472.py
@@ -0,0 +1,47 @@
+# Problem: Grading Students - https://www.hackerrank.com/challenges/grading/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    # Write your code here
+    rounded_grades = []
+    for grade in grades:
+        if grade < 38:
+            rounded_grades.append(grade)
+        else:
+            next_multiple_of_5 = (grade // 5 + 1) * 5
+            if next_multiple_of_5 - grade < 3:
+                rounded_grades.append(next_multiple_of_5)
+            else:
+                rounded_grades.append(grade)
+    return rounded_grades
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
