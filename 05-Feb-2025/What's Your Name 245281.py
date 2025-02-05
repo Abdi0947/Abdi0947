630388a161807283137bc6cacf747f7c041d47b5
Abdi Debela <122887024+Abdi0947@users.noreply.github.com>
2025-02-06 00:30:02 +0300

    Created 'What's Your Name' by A2SV Hub

---
+++ 05-Feb-2025/What's Your Name 245281.py
@@ -0,0 +1,9 @@
+# Problem: What's Your Name - https://www.hackerrank.com/challenges/whats-your-name/problem?isFullScreen=true
+
+def print_full_name(first, last):
+    print(f"Hello {first} {last}! You just delved into python.")
+     
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
