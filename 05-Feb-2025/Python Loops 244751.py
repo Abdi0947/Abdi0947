353e1e889e147502ee4a3fbbce29f0330b7eaa52
Abdi Debela <122887024+Abdi0947@users.noreply.github.com>
2025-02-05 20:53:00 +0300

    Created 'Python Loops' by A2SV Hub

---
+++ 05-Feb-2025/Python Loops 244751.py
@@ -0,0 +1,8 @@
+# Problem: Python Loops - https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        m=pow(i,2)
+        print(m)
+  
