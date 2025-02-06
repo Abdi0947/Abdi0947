7f829076b9aa649cb7741786043179433c9bf382
Abdi Debela <122887024+Abdi0947@users.noreply.github.com>
2025-02-06 12:03:39 +0300

    Created 'Python Division' by A2SV Hub

---
+++ 06-Feb-2025/Python Division 246202.py
@@ -0,0 +1,11 @@
+# Problem: Python Division - https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+
+if __name__ == '__main__':  
+    a = int(input()) 
+    b = int(input())  
+    
+    result1 = a // b  
+    result2 = a / b  
+    
+    print(result1)
+    print(result2)  
