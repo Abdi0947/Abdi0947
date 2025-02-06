45b85b240e772234cdab0ba031271800aa3e0890
Abdi Debela <122887024+Abdi0947@users.noreply.github.com>
2025-02-06 19:40:02 +0300

    Created 'Python string-split-and-join' by A2SV Hub

---
+++ 06-Feb-2025/Python string-split-and-join 247661.py
@@ -0,0 +1,13 @@
+# Problem: Python string-split-and-join - https://www.hackerrank.com/challenges/python-string-split-and-join/problem?isFullScreen=true
+
+
+
+def split_and_join(line):
+    # write your code here
+    line=line.split()
+    return '-'.join(line)
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
