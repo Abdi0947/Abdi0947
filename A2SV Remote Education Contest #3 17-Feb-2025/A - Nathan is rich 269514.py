d7bacd15fd1b1772514e485a700db1173431c81f
Abdi Debela <122887024+Abdi0947@users.noreply.github.com>
2025-02-17 20:56:33 +0300

    Created 'A - Nathan is rich' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/A - Nathan is rich 269514.py
@@ -0,0 +1,17 @@
+# Problem: A - Nathan is rich - https://codeforces.com/gym/588094/problem/A
+
+for i in range(int(input())):
+    m=input()
+    val=int(m)
+    if val>=4:
+        car=val//4
+        remain=val%4
+        if remain >=4:
+            car+=remain//4
+        elif remain >=2:
+            car+=remain//2
+    else:
+        car=1
+    print(car)
+    
+    
