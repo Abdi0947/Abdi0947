75d88229deba6b5727189dd3d580477efb496a0b
Abdi Debela <122887024+Abdi0947@users.noreply.github.com>
2025-02-18 14:08:32 +0300

    Created 'D - Adjacents, ewww !' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 18-Feb-2025/D - Adjacents, ewww ! 270784.py
@@ -0,0 +1,33 @@
+# Problem: D - Adjacents, ewww ! - https://codeforces.com/gym/588094/problem/D
+
+def solve():
+    n = int(input())
+
+    if n == 1:
+        print(1)
+        return
+    elif n == 2:
+        print(-1)
+        return
+
+    matrix = [[0] * n for _ in range(n)]
+    num = 1
+    
+    for i in range(n):
+        for j in range(n):
+            if (i + j) % 2 == 0:  
+                matrix[i][j] = num
+                num += 1
+    for i in range(n):
+        for j in range(n):
+            if matrix[i][j] == 0:  
+                matrix[i][j] = num
+                num += 1
+
+    for row in matrix:
+        print(*row)
+
+
+t = int(input())
+for _ in range(t):
+    solve()
