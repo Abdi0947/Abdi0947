3c1c44f5417e441bc6ff0d8d6fcb9d1f2d25d8a4
Abdi Debela <122887024+Abdi0947@users.noreply.github.com>
2025-02-18 13:50:07 +0300

    Created 'B - Card Game' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 18-Feb-2025/B - Card Game 270769.py
@@ -0,0 +1,8 @@
+# Problem: B - Card Game - https://codeforces.com/gym/588094/problem/B
+
+n = int(input())
+arr = list(map(int, input().split()))
+pairs= [(val, key + 1) for key, val in enumerate(arr)]
+pairs.sort()
+for i in range(n//2):
+    print(pairs[i][1], pairs[n - i - 1][1])
