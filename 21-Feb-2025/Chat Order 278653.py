8b2c3be424c01da9cb3202f816a8c8565d4e7f7d
Abdi Debela <122887024+Abdi0947@users.noreply.github.com>
2025-02-21 16:17:04 +0300

    Created 'Chat Order' by A2SV Hub

---
+++ 21-Feb-2025/Chat Order 278653.py
@@ -0,0 +1,13 @@
+# Problem: Chat Order - https://codeforces.com/problemset/problem/637/B
+
+n = int(input())
+nums = [input().strip() for _ in range(n)] 
+
+seen = set() 
+arr = []  
+
+for num in reversed(nums):
+    if num not in seen:
+        seen.add(num)
+        arr.append(num)
+print("\n".join(arr))
