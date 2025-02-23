83a29b30efd391b558218307baf321bebbaf8774
Abdi Debela <122887024+Abdi0947@users.noreply.github.com>
2025-02-23 15:51:00 +0300

    Created 'B - The best for Geleta' by A2SV Hub

---
+++ A2SV Remote Contest #4 23-Feb-2025/B - The best for Geleta 281086.py
@@ -0,0 +1,26 @@
+# Problem: B - The best for Geleta - https://codeforces.com/gym/590201/problem/B
+
+def process_test_cases():
+    n = int(input())  
+    for _ in range(n):
+        
+        n, m = map(int, input().split())
+ 
+        a = list(map(int, input().split()))
+    
+        max_a = max(a)
+        ans = []
+        
+        for _ in range(m):
+            op, l, r = input().split()
+            l = int(l)
+            r = int(r)
+            if op == '+':
+                if l <= max_a <= r:
+                    max_a += 1
+            elif op == '-':
+                if l <= max_a <= r:
+                    max_a -= 1
+            ans.append(str(max_a))
+        print(' '.join(ans))
+process_test_cases()
