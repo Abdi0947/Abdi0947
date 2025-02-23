c0010120397192da304768e0ec598aef9aed8a81
Abdi Debela <122887024+Abdi0947@users.noreply.github.com>
2025-02-23 22:20:10 +0300

    Created 'D - Great Photographer' by A2SV Hub

---
+++ A2SV Remote Contest #4 23-Feb-2025/D - Great Photographer 281267.py
@@ -0,0 +1,24 @@
+# Problem: D - Great Photographer - https://codeforces.com/gym/590201/problem/D
+
+def bob_m(answer, m):
+    L = max(ans[0] for ans in answer)
+    H = min(ans[1] for ans in answer)
+
+    if L > H:
+        return -1
+    elif m < L:
+        return L - m
+    elif m > H:
+        return m - H
+    else:
+        return 0
+
+k, m = input().split()
+k = int(k)
+m = int(m)
+answer = []
+for _ in range(k):
+    a, b = input().split()
+    answer.append((min(int(a), int(b)), max(int(a), int(b))))
+res = bob_m(answer, m)
+print(res)
