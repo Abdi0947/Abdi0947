3c37cf3fe680f0b7f74cbb82e8c6089f5f26c983
Abdi Debela <122887024+Abdi0947@users.noreply.github.com>
2025-02-23 15:50:47 +0300

    Created 'A - Harmonic Melody' by A2SV Hub

---
+++ A2SV Remote Contest #4 23-Feb-2025/A - Harmonic Melody 281085.py
@@ -0,0 +1,17 @@
+# Problem: A - Harmonic Melody - https://codeforces.com/gym/590201/problem/A
+
+def is_perfect_melody(notes):
+    for i in range(len(notes) - 1):
+        interval = abs(notes[i] - notes[i + 1])
+        if interval not in {5, 7}:
+            return False
+    return True
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    notes = list(map(int, input().split()))
+    if is_perfect_melody(notes):
+        print("YES")
+    else:
+        print("NO")
