45fb3c7d079abcfb9a2e2b4e197fefe4df4178a5
Abdi Debela <122887024+Abdi0947@users.noreply.github.com>
2025-02-24 15:18:59 +0300

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 24-Feb-2025/A. Beautiful Matrix 281616.py
@@ -0,0 +1,13 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+matrix = [list(map(int, input().split())) for _ in range(5)]
+
+for i in range(5):
+    for j in range(5):
+        if matrix[i][j] == 1:
+            row, col = i, j
+            break
+row_moves = abs(row - 2)  
+col_moves = abs(col - 2) 
+
+print(row_moves + col_moves)
