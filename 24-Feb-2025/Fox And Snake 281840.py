211d9c6aa1233496ab7b0215c76d6aa751d541ba
Abdi Debela <122887024+Abdi0947@users.noreply.github.com>
2025-02-24 20:28:10 +0300

    Created 'Fox And Snake' by A2SV Hub

---
+++ 24-Feb-2025/Fox And Snake 281840.py
@@ -0,0 +1,24 @@
+# Problem: Fox And Snake - https://codeforces.com/problemset/problem/510/A
+
+n,m=map(int,(input().split()))
+if n%2 ==0:
+  print("enter n odd number")
+  n,m=map(int(input().split()))
+row=1
+isLast=True
+for i in range(n):
+  if i%2==0:
+       print("#"*m)
+  else:
+    if isLast:
+        print("."*(m-1)+"#")
+        isLast=False
+    else:
+        print("#"+"."*(m-1))
+        isLast=True
+       
+       
+
+  
+
+  
