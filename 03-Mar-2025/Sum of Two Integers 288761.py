e6bb120c242ca06af33295346ed2f5e42c2e5682
Abdi Debela <122887024+Abdi0947@users.noreply.github.com>
2025-03-03 14:31:39 +0300

    Created 'Sum of Two Integers' by A2SV Hub

---
+++ 03-Mar-2025/Sum of Two Integers 288761.py
@@ -0,0 +1,15 @@
+# Problem: Sum of Two Integers - https://leetcode.com/problems/sum-of-two-integers/description/
+
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        return a+b
+        
+sol=Solution()
+a = 1
+b = 2
+print(sol.getSum(a,b))
